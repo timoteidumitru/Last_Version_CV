e2d2f21c1a61118de58cc59d38e410445eb05315
Timotei Dumitru <dumitru.timy@gmail.com>
2018-12-07 12:41:03 +0000

    initial setup

---
+++ src/components/main/Index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import 'index.scss'
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Index
