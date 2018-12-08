9ca3d925bef6b7058330042184855b63defe6a7a
Timotei Dumitru <dumitru.timy@gmail.com>
2018-12-08 22:04:46 +0000

    Updates

---
+++ precache-manifest.addf25deebee980423da35b1c1e5beaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b53bbe63faad37802d92",
+    "url": "/last-version-cv/static/js/runtime~main.b53bbe63.js"
+  },
+  {
+    "revision": "adb1bfc18523f1cc5233",
+    "url": "/last-version-cv/static/js/main.adb1bfc1.chunk.js"
+  },
+  {
+    "revision": "b61f9f0b70fcc34a85f9",
+    "url": "/last-version-cv/static/js/1.b61f9f0b.chunk.js"
+  },
+  {
+    "revision": "adb1bfc18523f1cc5233",
+    "url": "/last-version-cv/static/css/main.08ab11ac.chunk.css"
+  },
+  {
+    "revision": "756f447818fdfc1e30e1ca004bbdfb05",
+    "url": "/last-version-cv/index.html"
+  }
+];
