ccddc759329246baa0ebcfffc4018e977b6bee31
Timotei Dumitru <dumitru.timy@gmail.com>
2018-12-11 18:29:39 +0000

    finish to style menu page as designer ask plus some animations added to social icons

--- src/components/menu/MenuList.js
+++ src/components/menu/MenuList.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './menu.scss'
 import SocialIcons from './SocialIcons';
 
-function MenuList() {
-  return (
-    <div>
-      <ul className="menu-list">
-        <li className="menu-item">Work</li>
-        <li className="menu-item">About</li>
-        <li className="menu-item">Resume</li>
-        <li className="menu-item">Contact</li>
-        <SocialIcons />
-      </ul>
-    </div>
-  )
+class MenuList extends Component {
+
+  mouseEnter = () => {
+    const icons = document.querySelector(".social-icons")
+    icons.classList.add("fade-element")
+  }
+  mouseLeave = () => {
+    const icons = document.querySelector(".social-icons")
+    icons.classList.remove("fade-element")
+  }
+
+  render() {
+    return (
+      <div>
+        <ul className="menu-list">
+          <li onMouseEnter={this.mouseEnter}
+              onMouseLeave={this.mouseLeave} className="menu-item">Work</li>
+          <li onMouseEnter={this.mouseEnter}
+              onMouseLeave={this.mouseLeave} className="menu-item">About</li>
+          <li onMouseEnter={this.mouseEnter}
+              onMouseLeave={this.mouseLeave} className="menu-item">Resume</li>
+          <li onMouseEnter={this.mouseEnter}
+              onMouseLeave={this.mouseLeave} className="menu-item">Contact</li>
+          <SocialIcons />
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default MenuList
