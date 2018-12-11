6868ad5049675ee1cef02bfd65b82d63daaf22e2
Timotei Dumitru <dumitru.timy@gmail.com>
2018-12-11 12:29:14 +0000

    implement the links for each social icon

--- src/components/menu/SocialIcons.js
+++ src/components/menu/SocialIcons.js
@@ -5,10 +5,10 @@ function SocialIcons() {
   return (
     <div className="social-section">
       <ul className="social-icons">
-        <li className="social-icon"><img src="./images/social-icons/linkedin.png" alt="LinkedIn"/></li>
-        <li className="social-icon"><img src="./images/social-icons/github.png" alt="Github"/></li>
-        <li className="social-icon"><img src="./images/social-icons/facebook.png" alt="Facebook"/></li>
-        <li className="social-icon"><img src="./images/social-icons/googlePlus.png" alt="GooglePlus"/></li>
+        <li className="social-icon"><a href="https://www.linkedin.com/in/timotei-dumitru-1494ab160/" target="blank"><img src="./images/social-icons/linkedin.png" alt="LinkedIn"/></a></li>
+        <li className="social-icon"><a href="https://github.com/ShokadinuEU" target="_blank"><img src="./images/social-icons/github.png" alt="Github"/></a></li>
+        <li className="social-icon"> <a href="https://www.facebook.com/dumitru.timy" target="blank"><img src="./images/social-icons/facebook.png" alt="Facebook"/></a></li>
+        <li className="social-icon"><a href="https://plus.google.com/u/0/112277218680998369339" target="_blank"><img src="./images/social-icons/googlePlus.png" alt="GooglePlus"/></a></li>
       </ul>
     </div>
   )
