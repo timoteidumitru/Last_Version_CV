a4e1d406f62bd0374774118951213e68c1991362
Timotei Dumitru <dumitru.timy@gmail.com>
2019-06-06 20:38:56 +0100

    Updates

---
+++ precache-manifest.6923b91252844c68231c5c71e258e30f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff6a7d78f1b588520512cde8cec2fa2e",
+    "url": "/Last_Version_CV/static/media/about-image.ff6a7d78.jpg"
+  },
+  {
+    "revision": "ec179346d15c0571ba8f",
+    "url": "/Last_Version_CV/static/js/runtime~main.ec179346.js"
+  },
+  {
+    "revision": "0402ef440a3fc35a0f4c",
+    "url": "/Last_Version_CV/static/js/main.0402ef44.chunk.js"
+  },
+  {
+    "revision": "f402d03048464c69daf6",
+    "url": "/Last_Version_CV/static/js/1.f402d030.chunk.js"
+  },
+  {
+    "revision": "0402ef440a3fc35a0f4c",
+    "url": "/Last_Version_CV/static/css/main.6fffdbd2.chunk.css"
+  },
+  {
+    "revision": "8a50dc2a062f56e2dfa4d367af795eae",
+    "url": "/Last_Version_CV/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Last_Version_CV/precache-manifest.0e868d3697ba57e771e3557ccae18f57.js"
+  "/Last_Version_CV/precache-manifest.6923b91252844c68231c5c71e258e30f.js"
 );
 
 workbox.clientsClaim();
