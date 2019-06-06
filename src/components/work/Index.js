919ff1304bcaf66efcb4fb5dffcf4bdfbfe4035f
Timotei Dumitru <dumitru.timy@gmail.com>
2019-06-06 18:44:05 +0100

    add the structure of my work section

--- src/components/work/Index.js
+++ src/components/work/Index.js
@@ -4,8 +4,88 @@ import './work.scss'
 export default class Index extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello from Work Section</h1>
+      <div id="work">
+        <h1 className="lg-heading">
+          My
+          <span className="text-secondary">Work</span>
+        </h1>
+        <h2 className="sm-heading">
+          Check out some of my projects!
+        </h2>
+        <div className="projects">
+          {/* <!-- First Item --> */}
+          <div className="item">
+            <a href="#!">
+              <img className="project-img" src="img/projects/fashion-today.jpg" alt="Latest Project" />
+            </a>
+            <a href="#" className="btn-light">
+              <i className="fas fa-eye"></i> Fashion Today
+            </a>
+            <a href="https://shokadinueu.github.io/fashion-today/" target="_blank" className="btn-dark">
+              <i className="fab fa-github"> GitHub </i> 
+            </a>
+          </div>
+          {/* <!-- Second Item --> */}
+          <div className="item">
+            <a href="#!">
+              <img className="project-img" src="img/projects/development-website.jpg" alt="Development Website" />
+            </a>
+            <a href="#" className="btn-light">
+              <i className="fas fa-eye"></i> Development Website
+            </a>
+            <a href="https://shokadinueu.github.io/development-website/" target="_blank" className="btn-dark">
+              <i className="fab fa-github"> GitHub </i> 
+            </a>
+          </div>
+          {/* <!-- Third Item --> */}
+          <div className="item">
+            <a href="#!">
+              <img className="project-img" src="img/projects/weather-app.jpg" alt="Get Weather APP" />
+            </a>
+            <a href="#" className="btn-light">
+              <i className="fas fa-eye"></i> Get Weather App
+            </a>
+            <a href="https://shokadinueu.github.io/get-weather-app/" target="_blank" className="btn-dark">
+              <i className="fab fa-github">  GitHub</i>
+            </a>
+          </div>
+          {/* <!-- Fourth Item --> */}
+          <div className="item">
+            <a href="#!">
+              <img className="project-img" src="img/projects/type-n-speak.jpg" alt="Type-n-Speak" />
+            </a>
+            <a href="#" className="btn-light">
+              <i className="fas fa-eye"></i> Type-n-Speak
+            </a>
+            <a href="https://shokadinueu.github.io/type_n_speak_app/" target="_blank" className="btn-dark">
+              <i className="fab fa-github"></i> GitHub
+            </a>
+          </div>
+          {/* <!-- Fifth Item --> */}
+          <div className="item">
+            <a href="#!">
+              <img className="project-img" src="img/projects/apple-stock.jpg" alt="Apple Stock" />
+            </a>
+            <a href="#" className="btn-light">
+              <i className="fas fa-eye"></i> Apple Stock App
+            </a>
+            <a href="https://shokadinueu.github.io/apple-stock-info/" target="_blank" className="btn-dark">
+              <i className="fab fa-github"> GitHub </i> 
+            </a>
+          </div>
+          {/* <!-- Sixth Item --> */}
+          <div className="item">
+            <a href="#!">
+              <img className="project-img" src="img/projects/first-cv-online.jpg" alt="First CV" />
+            </a>
+            <a href="#" className="btn-light">
+              <i className="fas fa-eye"></i> First CV
+            </a>
+            <a href="https://shokadinueu.github.io/my-CV" target="_blank" className="btn-dark">
+              <i className="fab fa-github"></i> GitHub
+            </a>
+          </div>
+        </div>
       </div>
     )
   }
