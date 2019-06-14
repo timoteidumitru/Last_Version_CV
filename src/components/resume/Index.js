790faa276cf886efe6c6c97ffc93caf8191be5ed
Timotei Dumitru <dumitru.timy@gmail.com>
2019-06-14 19:50:15 +0100

    start to work on Resume section, inserting top nav button

--- src/components/resume/Index.js
+++ src/components/resume/Index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import TopNav from '../about/NavAbout'
 import './resume.scss'
 
 export default class Index extends Component {
   render() {
     return (
       <div>
+        <TopNav />
         <h1>Hello from Resume Section</h1>
       </div>
     )
