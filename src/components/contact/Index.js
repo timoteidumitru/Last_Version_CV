70ab25adf77fddc36e0a7e96f0f2ac79290ac29c
Timotei Dumitru <dumitru.timy@gmail.com>
2019-06-26 21:01:15 +0100

    implement the masic structure and style of contact section

--- src/components/contact/Index.js
+++ src/components/contact/Index.js
@@ -4,8 +4,34 @@ import './contact.scss'
 export default class Index extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello from Contact Section</h1>
+      <div className="contact-main">
+        <div className="contact-wrapper">
+          <div className="contact-header">
+            <h2 className="contact-title">Contact</h2>
+            <p className="contact-subtitle">
+              Have a question for me or want to work together?
+            </p>
+          </div>
+          <div className="contact-from">
+            <input 
+              type="text" 
+              className="contact-form-name"
+              placeholder="Name"
+              />
+            <input 
+              type="text" 
+              className="contact-form-email"
+              placeholder="Your email"
+              />
+            <textarea 
+              className="contact-form-textarea"
+              placeholder="Write your message here..."
+              rows="6"
+              cols="35"
+              >
+            </textarea>
+          </div>
+        </div>
       </div>
     )
   }
