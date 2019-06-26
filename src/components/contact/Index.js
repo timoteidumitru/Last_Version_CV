f7164e1d25d164e3c300b60c2dddea2d325e2841
Timotei Dumitru <dumitru.timy@gmail.com>
2019-06-27 00:50:00 +0100

    add a google map to show my location on contact section

--- src/components/contact/Index.js
+++ src/components/contact/Index.js
@@ -16,29 +16,34 @@ export default class Index extends Component {
             </p>
           </div>
           <div className="contact-from">
-            <input 
-              type="text" 
-              className="contact-form-name"
-              placeholder="Name"
-              />
-            <input 
-              type="text" 
-              className="contact-form-email"
-              placeholder="Your email"
-              />
-            <textarea 
-              className="contact-form-textarea"
-              placeholder="Write your message here..."
-              rows="6"
-              cols="35"
-              >
-            </textarea>
-            <button className="contact-form-button">
-              Submit
-            </button>
+            <div className="contact-message">
+              <input 
+                type="text" 
+                className="contact-form-name"
+                placeholder="Name"
+                />
+              <input 
+                type="text" 
+                className="contact-form-email"
+                placeholder="Your email"
+                />
+              <textarea 
+                className="contact-form-textarea"
+                placeholder="Write your message here..."
+                rows="6"
+                cols="35"
+                >
+              </textarea>
+              <button className="contact-form-button">
+                Submit
+              </button>
+            </div>
+            <div className="contact-map">
+              <iframe title="my location" width='500px' height='350px' id='mapcanvas' src='https://maps.google.com/maps?q=London,%20United%20Kingdom&Roadmap&z=10&ie=UTF8&iwloc=&output=embed' frameBorder='0' scrolling='no' marginHeight='0' marginWidth='0' />
+            </div> 
           </div>
-          <Footer />
         </div>
+        <Footer />
       </div>
     )
   }
