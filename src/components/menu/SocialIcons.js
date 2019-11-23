0b5d8f14ab4c620664e69fe719430f89db74d20c
Timotei Dumitru <dumitru.timy@gmail.com>
2019-11-23 15:25:28 +0000

    re-write some content on menu page

--- src/components/menu/SocialIcons.js
+++ src/components/menu/SocialIcons.js
@@ -1,17 +1,40 @@
-import React from 'react'
-import './menu.scss'
+import React from "react";
+import "./menu.scss";
 
 function SocialIcons() {
   return (
     <div className="social-section">
       <ul className="social-icons">
-        <li className="social-icon"><a href="https://www.linkedin.com/in/timotei-dumitru-1494ab160/" target="blank" rel="noopener noreferrer"><img src="./images/social-icons/linkedin.png" alt="L"/></a></li>
-        <li className="social-icon"><a href="https://github.com/ShokadinuEU" target="_blank" rel="noopener noreferrer"><img src="./images/social-icons/github.png" alt="G"/></a></li>
-        <li className="social-icon"> <a href="https://www.facebook.com/dumitru.timy" target="blank" rel="noopener noreferrer"><img src="./images/social-icons/facebook.png" alt="F"/></a></li>
-        <li className="social-icon"><a href="https://plus.google.com/u/0/112277218680998369339" target="_blank" rel="noopener noreferrer"><img src="./images/social-icons/googlePlus.png" alt="G"/></a></li>
+        <li className="social-icon">
+          <a
+            href="https://www.linkedin.com/in/timotei-dumitru-1494ab160/"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            <img src="./images/social-icons/linkedin.png" alt="L" />
+          </a>
+        </li>
+        <li className="social-icon">
+          <a
+            href="https://github.com/ShokadinuEU"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="./images/social-icons/github.png" alt="G" />
+          </a>
+        </li>
+        <li className="social-icon">
+          <a
+            href="https://shokadinueu.github.io/phase-eight/"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            <img src="./images/social-icons/portofolio.png" alt="P" />
+          </a>
+        </li>
       </ul>
     </div>
-  )
+  );
 }
 
-export default SocialIcons
+export default SocialIcons;
