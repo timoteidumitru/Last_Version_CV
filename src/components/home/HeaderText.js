0e536fc8db9353a8244145d4228a8d7d48195460
Timotei Dumitru <dumitru.timy@gmail.com>
2020-03-19 21:58:02 +0000

    edit the text on the main page

--- src/components/home/HeaderText.js
+++ src/components/home/HeaderText.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import './header.scss'
+import React from "react";
+import "./header.scss";
 
 export default function HeaderText() {
   return (
     <div>
       <div className="header-text">
-        Hello,<br />
-        I'm Timotei,<br />
-        a Junior<br />
-        Front End<br />
-        Web<br />
+        Hello,
+        <br />
+        I'm Timotei,
+        <br />
+        a Front-End
+        <br />
+        Web
+        <br />
         Developer
-      <span className="based-in-london">based in London</span>
+        <span className="based-in-london">based in London</span>
       </div>
     </div>
-  )
+  );
 }
