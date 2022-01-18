3b85f4b01385243613a8bf5cedaf0aa59f0d783c
Timotei Dumitru <dumitru.timy@gmail.com>
2022-01-18 12:59:22 +0000

    feat: add form validation to interface level

--- src/components/contact/Index.js
+++ src/components/contact/Index.js
@@ -1,4 +1,6 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
+import { Formik, Field } from "formik";
+import * as Yup from "yup";
 import emailjs from "emailjs-com";
 import TopNav from "../about/TopNav";
 import Footer from "../about/Footer";
@@ -6,7 +8,18 @@ import "./contact.scss";
 
 const Index = () => {
   const form = useRef();
-  const [confirmMsg, setConfirmMsg] = useState("");
+  // const [confirmMsg, setConfirmMsg] = useState("");
+
+  const SignupSchema = Yup.object().shape({
+    user_name: Yup.string()
+      .min(3, "Too Short name!")
+      .max(50, "Too Long name!")
+      .required("Name Required"),
+    user_email: Yup.string()
+      .email("Invalid email adress")
+      .required("Email Required"),
+  });
+
   const sendEmail = (e) => {
     e.preventDefault();
 
@@ -20,13 +33,13 @@ const Index = () => {
       .then(
         (result) => {
           console.log(result.text);
-          if (result.text === "OK") {
-            setConfirmMsg("Message was sent successfully!");
-          } else {
-            setConfirmMsg(
-              "Message was not sent successfully, please try again."
-            );
-          }
+          // if (result.text === "OK") {
+          //   setConfirmMsg("Message was sent successfully!");
+          // } else {
+          //   setConfirmMsg(
+          //     "Message was not sent successfully, please try again."
+          //   );
+          // }
         },
         (error) => {
           console.log(error.text);
@@ -46,39 +59,72 @@ const Index = () => {
           </p>
         </div>
         <div className="contact-from">
-          <form ref={form} onSubmit={sendEmail} className="contact-message">
-            {confirmMsg && (
-              <p
-                className={
-                  confirmMsg.includes("successfuly")
-                    ? "unsuccess-msg"
-                    : "success-msg"
-                }
-              >
-                {confirmMsg}
-              </p>
+          <Formik
+            initialValues={{ user_name: "", user_email: "" }}
+            validationSchema={SignupSchema}
+          >
+            {({ isSubmitting, errors, touched }) => (
+              <form ref={form} onSubmit={sendEmail} className="contact-message">
+                {/* {confirmMsg && (
+                  <p
+                    className={
+                      confirmMsg.includes("successfuly")
+                        ? "unsuccess-msg"
+                        : "success-msg"
+                    }
+                  >
+                    {confirmMsg}
+                  </p>
+                )} */}
+                {errors.user_name && touched.user_name ? (
+                  <div className="unsuccess-msg">{errors.user_name}</div>
+                ) : null}
+                <Field
+                  name="user_name"
+                  className="contact-form-name"
+                  placeholder="Enter a name.."
+                />
+                {/* <input
+                  type="text"
+                  className="contact-form-name"
+                  placeholder="Enter a name.."
+                  name="user_name"
+                /> */}
+                {/* <ErrorMessage
+                  name="user_email"
+                  component="div"
+                  className={
+                    confirmMsg.includes("successfuly")
+                      ? "unsuccess-msg"
+                      : "success-msg"
+                  }
+                /> */}
+                {errors.user_email && touched.user_email ? (
+                  <div className="unsuccess-msg">{errors.user_email}</div>
+                ) : null}
+                <Field
+                  name="user_email"
+                  placeholder="Enter email address.."
+                  className="contact-form-email"
+                  type="email"
+                />
+                {/* <input
+                  type="text"
+                  className="contact-form-email"
+                  placeholder="Your email"
+                  name="user_email"
+                /> */}
+                <textarea
+                  name="message"
+                  className="contact-form-textarea"
+                  placeholder="Write your message here..."
+                  rows="6"
+                  cols="35"
+                ></textarea>
+                <button className="contact-form-button">Submit</button>
+              </form>
             )}
-            <input
-              type="text"
-              className="contact-form-name"
-              placeholder="Name"
-              name="user_name"
-            />
-            <input
-              type="text"
-              className="contact-form-email"
-              placeholder="Your email"
-              name="user_email"
-            />
-            <textarea
-              name="message"
-              className="contact-form-textarea"
-              placeholder="Write your message here..."
-              rows="6"
-              cols="35"
-            ></textarea>
-            <button className="contact-form-button">Submit</button>
-          </form>
+          </Formik>
           <div className="contact-map">
             <iframe
               title="my location"
